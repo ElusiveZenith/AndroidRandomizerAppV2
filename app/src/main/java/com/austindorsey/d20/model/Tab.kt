69b10faf3c21d4c38e16894d759c1871ec369381
Austin Dorsey <austin_dorsey@hotmail.com>
2020-07-03 20:48:12 -0500

    Cleaning up unneeded code.

--- app/src/main/java/com/austindorsey/d20/model/Tab.kt
+++ app/src/main/java/com/austindorsey/d20/model/Tab.kt
@@ -2,6 +2,4 @@ package com.austindorsey.d20.model
 
 import androidx.fragment.app.Fragment
 
-class Tab (val name: String, val fragment: Fragment) {
-
-}
+class Tab (val name: String, val fragment: Fragment)
--- app/src/main/java/com/austindorsey/d20/util/TabAdapter.kt
+++ app/src/main/java/com/austindorsey/d20/util/TabAdapter.kt
@@ -3,8 +3,6 @@ package com.austindorsey.d20.util
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.austindorsey.d20.fragments.CustomDiceNumaricFragment
-import com.austindorsey.d20.fragments.CustomDiceStringFragment
 import com.austindorsey.d20.model.Tab
 
 class TabAdapter(fm: FragmentManager, private var tabs: List<Tab>) : FragmentStatePagerAdapter(fm, FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
@@ -24,5 +22,4 @@ class TabAdapter(fm: FragmentManager, private var tabs: List<Tab>) : FragmentSta
     override fun getCount(): Int {
         return tabs.size
     }
-
 }
